43b51940b83c574ceafefac324a0071ed3d659a0
leson <xxx>
2024-07-17 16:02:28 +0800

    leson

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,6 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
+
+export const experimental_ppr = true;     // 开启ppr（预渲染），Suspense包裹的组件视为动态的，next会在编译时自动确认静态与动态部分
  
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
