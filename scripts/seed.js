a788ba726dba6e8751d0ca793e8aca767256aaf5
leson <xxx>
2024-07-18 17:12:59 +0800

    leson

--- scripts/seed.js
+++ scripts/seed.js
@@ -173,7 +173,7 @@ async function main() {
 
 main().catch((err) => {
   console.error(
-    'An error occurred while attempting to seed the database:',
+    'An error occurred while attempting to  the database:',
     err,
   );
 });
